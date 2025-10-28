7f44575cfabdb2a3f2773a12ab689b8feb64c299
raihan <raihanamin2212@gmail.com>
2025-10-28 10:07:58 +0700

    bisa ayok

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/resume/',                              // Plan B
+  base: '/',
   build: { outDir: 'dist/resume', emptyOutDir: true },
   plugins: [vue()],
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } }
